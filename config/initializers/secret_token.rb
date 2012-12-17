38ed4d3627907c488e7d21a1f40ebf32ba536061
Denis Ivanov <visible.h4x@gmail.com>
2012-12-17 01:37:17 -0800

    intial rails generation output

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Motorstories::Application.config.secret_token = 'e3a4e68a214b36138ba95c19660d69c2f0e0a32eb39caf492ba6b83d97ca5fb3c918daab2a4c7c439fc2fd3eb9d27443eaf97642452ad09987668f6f52171153'
