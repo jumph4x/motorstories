e68578a753bfca5fc789ca46a9824a773a284e27
Denis Ivanov <visible.h4x@gmail.com>
2013-03-27 17:47:49 -0700

    Add Avatars to Users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130328002406) do
+ActiveRecord::Schema.define(:version => 20130328004504) do
 
   create_table "profiles", :force => true do |t|
     t.integer  "user_id"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(:version => 20130328002406) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
+    t.string   "avatar"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
