f57733474e6f8b83683ac03b042a45004e824a1f
Denis Ivanov <visible.h4x@gmail.com>
2013-04-04 17:37:07 -0700

    Landing page direction improved

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,15 @@ module ApplicationHelper
     browser.ie6?
   end
 
+  def html_title
+    methud = "html_title_#{controller.action_name}"
+    title = if respond_to? methud
+      send(methud)
+    else
+      'Built by Gearheads. For Gearheads.'
+    end
+
+    "#{title} | Motorstori.es"
+  end
+
 end
--- app/helpers/static_helper.rb
+++ app/helpers/static_helper.rb
@@ -1,2 +1,6 @@
 module StaticHelper
+  
+  def html_title_index
+    'Your Motor Projects Have a New Home'
+  end
 end
