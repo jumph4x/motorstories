851893bd44518c9d3b52ef46c439d33aadef0861
Denis Ivanov <visible.h4x@gmail.com>
2013-04-24 15:39:34 -0700

    Validation shorthand syntax

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,18 +9,14 @@ class User
          :recoverable, :rememberable, :trackable, :validatable
 
   key :email, String
-  key :name, String
-  key :username, String
+  key :name, String, :required => true
+  key :username, String, :required => true, :unique => true
   key :instagram_handle, String
   key :facebook_handle, String
   key :tumblr_handle, String
   key :location_id, Integer
 
   timestamps!
-
-  validates_presence_of :name
-  validates_presence_of :username
-  validates_uniqueness_of :username
   
   validate :location_validity
 
