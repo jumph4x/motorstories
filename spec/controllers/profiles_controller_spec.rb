82b0a4b3543ed85b73da4b511d7fffe75f7f79e6
Denis Ivanov <visible.h4x@gmail.com>
2013-04-28 17:32:51 -0700

    ProfilesController primer

--- spec/controllers/profiles_controller_spec.rb
+++ spec/controllers/profiles_controller_spec.rb
@@ -2,4 +2,14 @@ require 'spec_helper'
 
 describe ProfilesController do
 
+  context 'on #show' do
+    let(:profile){ create(:profile) }
+
+    it 'should render' do
+      get :show, :id => profile.username
+      response.code.should == '200'
+      response.should render_template(:show)
+    end
+  end
+
 end
--- spec/models/profile_spec.rb
+++ spec/models/profile_spec.rb
@@ -1,14 +1,26 @@
 require 'spec_helper'
 
 describe Profile do
+  let(:profile){ create(:profile) }
+
   it 'should load locations by id' do
-    profile = FactoryGirl.create(:profile)  
     profile.location.name.should be_present
 
     profile.location_id = nil
     profile.save.should be_false
     profile.errors.messages[:location_id].join.should match(/location/i)
     lambda{ profile.save! }.should raise_error
+  end
+
+  context 'for validations' do
+    it 'should require username' do
+      profile.username.should be_present
+
+      profile.username = nil
+      profile.save.should be_false
+      profile.errors.messages[:username].join.should match(/blank/i)
+      lambda{ profile.save! }.should raise_error
+    end
   end
 end
 
