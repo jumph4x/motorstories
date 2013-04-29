e748462eb4df3f27484e85bf9b07dd3e039b56ff
Denis Ivanov <visible.h4x@gmail.com>
2013-04-29 14:06:07 -0700

    Preparing SEO friendly nested routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Motorstories::Application.routes.draw do
 
   resources :profiles, :path => 'people'
-  resources :vehicles
+  resources :vehicles, :path => 'projects', :except => [:show, :index]
+
+  LOCATION_IDS ||= /#{Location.pluck(:slug).join('|')}/
+
+  match ':location_id/:make_id/:model_id/:year/:nickname' => 'vehicles#index', :location_id => LOCATION_IDS, :trailing_slash => true
+  match ':location_id/:make_id(/:model_id(/:year))' => 'vehicles#index', :location_id => LOCATION_IDS, :trailing_slash => true
+  match ':location_id' => 'locations#show', :location_id => LOCATION_IDS
 
   devise_for :users
-
   root :to => 'static#index'
 
 end
