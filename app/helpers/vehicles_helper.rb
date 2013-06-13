9dd0a20f64ad7a8d3df19991b814fe77d4c0f735
Denis Ivanov <visible.h4x@gmail.com>
2013-06-12 21:40:28 -0700

    Helper to populate dropdowns

--- app/helpers/vehicles_helper.rb
+++ app/helpers/vehicles_helper.rb
@@ -1,2 +1,14 @@
 module VehiclesHelper
+
+  SELECT_OPTIONS = {
+    :car => {
+      :project_type => ['autocross warrior', 'track attack', 'drifter', 'daily driver', 'offroader', 'hauler', 'vip']
+    },
+    :motorcycle => {
+      :project_type => ['track attack', 'adventure', 'street fighter', 'commuter', 'stunter']
+    }
+  }
+  def select_options_for vehicle_type, field
+    SELECT_OPTIONS[vehicle_type.to_sym][field.to_sym]
+  end
 end
--- spec/helpers/vehicles_helper_spec.rb
+++ spec/helpers/vehicles_helper_spec.rb
@@ -1,4 +1,10 @@
 require 'spec_helper'
 
 describe VehiclesHelper do
+
+  context 'on #select_options_for' do
+    it 'should return an array' do
+      helper.select_options_for('car', :project_type).should include('drifter')
+    end
+  end
 end
