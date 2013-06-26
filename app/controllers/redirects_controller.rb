91103e9071e4159e7566444c6d7413007801df1f
Denis Ivanov <visible.h4x@gmail.com>
2013-06-25 23:12:55 -0700

    Fixing to use new named routes

--- app/controllers/redirects_controller.rb
+++ app/controllers/redirects_controller.rb
@@ -3,9 +3,9 @@ class RedirectsController < ApplicationController
   def vehicles_index
     mmy = params.slice(:make_slug, :model_slug, :year)
     target = if (location = session[:location])
-      vehicles_path(mmy.merge(:location_slug => location))
+      semantic_vehicles_path(mmy.merge(:location_slug => location))
     else
-      vehicles_path(mmy.merge(:location_slug => 'all'))
+      semantic_vehicles_path(mmy.merge(:location_slug => 'all'))
     end
 
     redirect_to target
