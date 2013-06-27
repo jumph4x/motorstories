9fe96d3f7ed886519d36f6cdc00cafb82c66c221
Denis Ivanov <visible.h4x@gmail.com>
2013-06-26 19:17:24 -0700

    Bumping controller coverage

---
+++ spec/controllers/registrations_controller_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe Devise::RegistrationsController do
+  render_views
+  before{ @request.env["devise.mapping"] = Devise.mappings[:user] }
+
+  it 'renders #new' do
+    get :new
+    response.should render_template(:new)
+    response.code.should == '200'
+  end
+end
+
