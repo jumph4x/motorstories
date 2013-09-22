78677438f4e567a41c0221cf47d01fc81ecbbac8
visible.h4x@gmail.com <visible.h4x@gmail.com>
2013-09-21 22:08:05 -0700

    Sorting years for usability

--- app/models/base_vehicle.rb
+++ app/models/base_vehicle.rb
@@ -12,7 +12,7 @@ class BaseVehicle
 
   class << self
     def years_by_make_id_and_model_id make_id, model_id
-      BaseVehicle.where(:make_id => make_id, :model_id => model_id).distinct(:year)
+      BaseVehicle.where(:make_id => make_id, :model_id => model_id).distinct(:year).sort{|a,b| b <=> a }
     end
   end
 end
