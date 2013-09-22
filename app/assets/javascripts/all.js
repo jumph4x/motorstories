ec66132b581894adc1622aaf1fe9ac131a409432
visible.h4x@gmail.com <visible.h4x@gmail.com>
2013-09-21 22:05:18 -0700

    Tweaking the layout

--- app/assets/javascripts/all.js
+++ app/assets/javascripts/all.js
@@ -8,7 +8,9 @@
 //= require parsley
 //= require './mmy_selector'
 //= require './vehicles'
+//= require './jquery.fittext'
 
 $(document).ready(function(){
   $(document).foundation();
+  $("#hero h1").fitText(2.5, { minFontSize: '24px' });
 });
