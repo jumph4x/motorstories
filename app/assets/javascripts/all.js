b4bb6c1741e373543d3e8a34741fb257914ea1c5
visible.h4x@gmail.com <visible.h4x@gmail.com>
2013-09-23 19:23:48 -0700

    WIP: perfecting responsive layout

--- app/assets/javascripts/all.js
+++ app/assets/javascripts/all.js
@@ -13,4 +13,5 @@
 $(document).ready(function(){
   $(document).foundation();
   $("#hero h1").fitText(2.5, { minFontSize: '24px' });
+  $(".pitch").fitText(5, { minFontSize: '16px', maxFontSize: '35px' });
 });
