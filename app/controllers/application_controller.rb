f60f7ab299236ea899b4d10a049e226657222c83
visible.h4x@gmail.com <visible.h4x@gmail.com>
2013-10-13 20:26:41 -0700

    Extracting layout pieces into partials

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -32,7 +32,10 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_in_path_for(resource)
-    puts session.inspect
+    session[:previous_url] || root_path
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
     session[:previous_url] || root_path
   end
 end
