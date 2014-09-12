e40779820e602f8ab0fc608579b4312e73997f64
Denis Ivanov <visible@jumph4x.net>
2014-09-11 21:51:57 -0700

    Conditional tinter

--- app/assets/javascripts/all.js
+++ app/assets/javascripts/all.js
@@ -1,9 +1,11 @@
 //= require jquery
 //= require jquery_ujs
 //= require foundation
+//= require './jquery.fittext'
 //= require './mmy_selector'
 //= require './vehicles'
 
 $(document).ready(function(){
   $(document).foundation();
+  $('.fit').fitText(1.4);
 });
